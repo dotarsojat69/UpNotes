ebe55adcf1564e7210ce86fa50cb37de46d2c4e7
dotarsojat69 <tdeath457@gmail.com>
2024-09-24 22:42:26 +0700

    feat: update user info

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { getInitials } from '../utils/helper'
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
     <div className='flex items-center gap-3'>
-        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>{getInitials("Franco Freso")}</div>
+        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
+          {getInitials(userInfo?.fullName)}
+        </div>
 
         <div>
-            <p className='text-sm font-medium'>Franco Freso</p>
+            <p className='text-sm font-medium'>{userInfo?.fullName}</p>
             <button onClick={onLogout} className='text-sm text-slate-700 underline'>
                 Logout
             </button>
