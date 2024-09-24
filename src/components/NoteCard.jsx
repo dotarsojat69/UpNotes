1f95ff34a6a1c20f89c7890e8bcc97cdb7427259
dotarsojat69 <tdeath457@gmail.com>
2024-09-24 22:41:50 +0700

    feat: get all notes

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,7 +1,9 @@
+import moment from 'moment'
 import React from 'react'
 import { MdCreate, MdDelete, MdOutlinePushPin } from 'react-icons/md'
 
 const NoteCard = ({
+
     title,
     date,
     content,
@@ -19,7 +21,7 @@ const NoteCard = ({
                     {title}
                 </h6>
                     <span className='text-xs text-slate-500'>
-                        {date}
+                    {moment(date).format('Do MMM YYYY')}
                     </span>
             </div>
 
@@ -32,7 +34,7 @@ const NoteCard = ({
         <p className='text-xs text-slate-600 mt-2'>{content?.slice(0, 60)}</p>
 
         <div className='flex items-center justify-between mt-2'>
-            <div className='text-xs text-slate-500'>{tags}</div>
+            <div className='text-xs text-slate-500'>{tags.map((item) => `#${item}`)}</div>
 
             <div className='flex items-center gap-2'>
                 <MdCreate 
