6158675612da8e8e9929a2915c3048e0c6ee045a
dotarsojat69 <tdeath457@gmail.com>
2024-09-24 22:42:14 +0700

    feat: update user info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import ProfileInfo from './ProfileInfo'
 import { useNavigate } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
 
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   const onLogout = () => {
+    localStorage.clear()
     navigate("/login");
   };
 
@@ -33,7 +34,7 @@ const Navbar = () => {
         onClearSearch={onClearSearch}
         />
 
-        <ProfileInfo onLogout={onLogout}/>
+        <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>
     </div>
   )
 }
